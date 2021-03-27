5b01fa08e25cd3b78d138d90049edd6e6951088b
Kim Min-Young <48994053+my0614@users.noreply.github.com>
2021-03-27 09:57:31 +0900

    Create turtle_list.py
    리스트의 색상으로 삼각형그리기 (import turtle사용)

---
+++ turtle_list.py
@@ -0,0 +1,20 @@
+import turtle
+t = turtle.Turtle()
+t.shape("turtle")
+cursor=['blue','orange','red'] # 색상종류
+
+t.clear()
+t.color(cursor[0])
+t.stamp()
+t.fd(200)
+t.left(120)
+
+t.color(cursor[1])
+t.stamp()
+t.fd(200)
+t.left(120)
+
+t.color(cursor[2])
+t.stamp()
+t.fd(200)
+t.left(120)
